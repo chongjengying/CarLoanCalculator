c7b71c6a8bb239beae65b1482543159f623e7849
chongjengying <chongjy-pa16@student.tarc.edu.my>
2019-10-31 11:09:51 +0800

    loan

---
+++ app/src/main/java/com/example/carloancalculator/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.carloancalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        buttonCalculate.setOnClickListener{
+            calculateLoan()
+        }
+
+
+    }
+    private fun calculateLoan()
+    {
+        //Getting input data from the user
+        val car_price=editTextCarPrice.text.toString().toInt()
+        val down_payment=editTextDownPayment.text.toString().toInt()
+        val loan_period=editTextLoanPeriod.text.toString().toInt()
+        val interest_rate=editTextInterestRate.text.toString().toFloat()
+
+
+       val loan=car_price-down_payment
+        val interest=loan*4.5*5
+        val mrepayment=(loan +interest)/5/12
+
+        textViewCarLoan.text =getString(R.string.loan)+":${loan}"
+
+        textViewInterest.text=getString (R.string.interest)+":$(interest)"
+
+
+    }
+
+    fun resetInput(view: View)
+    {
+
+
+    }
+
+
+}
